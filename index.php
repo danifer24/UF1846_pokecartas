3568e79f1066b3acb539c8146940618d7229e7d1
danifer24 <danielvf24@gmail.com>
2025-04-09 15:07:26 +0200

    colores

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 
 function getPokemonData()
-{   
-    
+{
+
     // 1) genera número aleatorio
     $aleatorio = (string) random_int(1, 151);
     // 2) lee el contenido de la api 
@@ -19,8 +19,10 @@ function getPokemonData()
         "tipos" => array_map(fn($tipo) => ucwords($tipo['type']['name']), $poke_json["types"]),
         "habilidades" => array_map(fn($tipo) => ucwords($tipo['ability']['name']), $poke_json["abilities"])
     ];
-    $prob_shiny = 20;
-    if(rand(1,100) <= $prob_shiny) $pokemon["imagen"] = $poke_json["sprites"]["front_shiny"];
+    // si es shiny cambiamos la imagen, probabilidad de shiny 0.05 en este caso
+    $prob_shiny = 5;
+    if (rand(1, 100) <= $prob_shiny)
+        $pokemon["imagen"] = $poke_json["sprites"]["front_shiny"];
 
     return $pokemon;
 }
@@ -29,8 +31,34 @@ function getPokemonData()
 function renderCards($pokemon)
 {
     // recibe datos y genera el html
-    echo "<div class='carta fuego'>
-            <div class='img-container'>
+
+    //color segun tipo
+    echo "<div class='carta";
+    foreach ($pokemon["tipos"] as $tipo) {
+        switch ($tipo) {
+            case 'Water':
+                echo " agua";
+                break;
+
+            case 'Fire':
+                echo " fuego";
+                break;
+
+            case 'Grass':
+                echo " planta";
+                break;
+
+            case 'Electric':
+                echo " electrico";
+                break;
+
+            default:
+                break;
+        }
+    }
+    echo "'>";
+
+    echo "<div class='img-container'>
                 <img src=$pokemon[imagen]>
             </div>
             <div class='datos'>
@@ -65,9 +93,8 @@ function renderCards($pokemon)
 
 <body>
     <h1>PokeCartas</h1>
-
     <section id="pokecartas">
-        <div class="carta">
+        <!-- <div class="carta">
             <div class="img-container">
                 <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" alt="pikachu">
             </div>
@@ -82,16 +109,16 @@ function renderCards($pokemon)
                     <li>chispitas</li>
                 </ul>
             </div>
-        </div>
+        </div> -->
 
-        <?php for($i = 0; $i < 4; $i++){
-        renderCards(getPokemonData());
+        <?php for ($i = 0; $i < 4; $i++) {
+            renderCards(getPokemonData());
         } ?>
-        
+
     </section>
 
-    
-   
+
+
 </body>
 
 </html>
